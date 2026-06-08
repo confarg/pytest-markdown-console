14fb97899171e038e2dab824c1af5459bd367013
confarg <280620574+confarg@users.noreply.github.com>
2026-06-08 21:31:11 +0200

    chore: Add missing license header (#7)

--- src/pytest_markdown_console/__init__.py
+++ src/pytest_markdown_console/__init__.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 """pytest plugin for testing console code blocks in Markdown files."""
 
 from .plugin import markdown_console_tmpdir as markdown_console_tmpdir
